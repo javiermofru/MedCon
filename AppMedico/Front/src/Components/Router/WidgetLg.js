56227cbb3264ad88c6be83246561fd01897560ca
cirapozo00 <cirapozo00@gmail.com>
2022-04-03 14:47:43 +0200

    Co-authored-by: mvarasg99 <mvarasg99@users.noreply.github.com>

--- AppMedico/Front/src/Components/Router/WidgetLg.js
+++ AppMedico/Front/src/Components/Router/WidgetLg.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import "../Estilo/widgetLg.css" 
+import "../Estilo/widgetLg.css" ;
+import Notes from "./Notes";
+
 
 function WidgetLg() {
     const Button = ({type}) =>{
@@ -7,31 +9,7 @@ function WidgetLg() {
     }
     return (
         <div className="widgetLg">
-           <h3>Notas</h3> 
-           <table className='widgetLgTable'>
-               <tr className='widgetLgTr'>
-                   <th className='widgetLgTh'>Notas</th>
-                   <th className='widgetLgTh'>Estado</th>
-               </tr>
-               <tr className='widgetLgTr'>
-                   <td className='widgetLgNota'>
-                        <span className='Nota'>Pedir radiografía de Fco. Javier</span>
-                   </td>
-                   <td className='estado'><Button type="Urgente"/></td>
-               </tr>
-               <tr className='widgetLgTr'>
-                   <td className='widgetLgNota'>
-                        <span className='Nota'>Hacen falta guantes</span>
-                   </td>
-                   <td className='estado'><Button type="Nueva"/></td>
-               </tr>
-               <tr className='widgetLgTr'>
-                   <td className='widgetLgNota'>
-                        <span className='Nota'>Este mes tengo 10 días de vacaciones</span>
-                   </td>
-                   <td className='estado'><Button type="Info"/></td>
-               </tr>
-           </table>
+           <Notes />
            
         </div>
     );
