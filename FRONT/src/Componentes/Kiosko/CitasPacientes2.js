2d0a0b71b6d0760ea5f0ebee76833685ad066b11
Alejandro Mariscal Romero <alejandro.mariscal.romero@alumnos.upm.es>
2022-05-05 20:50:25 +0200

    link desde citas del paciente hasta el codigo aleatorio

--- FRONT/src/Componentes/Kiosko/CitasPacientes2.js
+++ FRONT/src/Componentes/Kiosko/CitasPacientes2.js
@@ -1,14 +1,18 @@
 import React, { Component, useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 //import { Button, ButtonGroup, Container, Table } from 'reactstrap';
 //import AppNavbar from './AppNavbar';
 import { Link, useParams } from 'react-router-dom';
 import Inputdni from './Inputdni';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCheck} from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
 
 export default function CitasPaciente(){
     const [citas, setCitas] = useState([]);
 
     let {dni} = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
     fetch(`http://localhost:8080/paciente/${dni}`)
@@ -17,6 +21,7 @@ export default function CitasPaciente(){
     }, [500]);
     console.log(`http://localhost:8080/paciente/${dni}`)
     console.log(citas);
+
 
     return (
         <div className='container'>
@@ -29,12 +34,22 @@ export default function CitasPaciente(){
                     <th>Registrar presencia</th>
                 </tr>
 
-                {citas.map(data => (
+                {citas.map((data,index) => (
                   <tr>
                     <td>{data.fecha}</td>
                     <td>{data.hora}</td>
-                    <td>{data.llamado}</td>
-                    <td>{data.registrado}</td>
+                    <td>{data.llamado ? 'SI' : 'NO'}</td>
+                    <td>
+                        <button 
+                            className='btn m-2 bg-success'
+                            onClick={() => navigate(`/paciente/codigo/${citas[index].id}`)}
+                            
+                        >
+                            <FontAwesomeIcon className='btn btn-success' icon={faCheck}/>
+
+                        </button>
+                    
+                    </td>
                   </tr>  
                 ))}
 
