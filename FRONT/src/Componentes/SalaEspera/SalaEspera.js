ee1b98f11e10d058e68bba0450942108dfb93f6f
Javier Moreno <javier.morenof@alumnos.upm.es>
2022-05-06 17:11:38 +0200

    Kiosko updated
    Co-authored-by: Alejandro Mariscal Romero <alexmariscalr@users.noreply.github.com>
    Co-authored-by: cirapozo00 <cirapozo00@users.noreply.github.com>
    Co-authored-by: Carlos Chinchilla Fiter <carlos-chinchilla@users.noreply.github.com>
    Co-authored-by: m.varasg99 <m.varasg99@users.noreply.github.com>

---
+++ FRONT/src/Componentes/SalaEspera/SalaEspera.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class SalaEspera extends Component {
+  render() {
+    return (
+      <div className='SalaEspera'>
+        <table>
+          <thead>
+              <tr>
+                <th>TURNO</th>
+                <th>SALA CONSULTA</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <td>AT228</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>JA828</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>JA828</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>JA828</td>
+              </tr>
+
+          </tbody>
+        </table>
+      
+      
+      
+      </div>
+    )
+  }
+}
